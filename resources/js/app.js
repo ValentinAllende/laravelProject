f244cb9bfad1ff13e9c0faadd5865c818311ddf7
ValentinAllende <valentin_allende@hotmail.com>
2023-05-06 16:01:46 -0300

    commit inicial

---
+++ resources/js/app.js
@@ -0,0 +1,4 @@
+import './bootstrap';
+import '../css/app/.css' 
+import * as bootstrap from 'bootstrap';
+$body-bg: #22222;
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+ <body>
+@extends('barra')
+
+@section('barra')
+    View one's barra
+@endsection
+
+
+    <h1>a</h1>
+ </body>
+</html>
